639f4c8cb9f80a98a58871732387d6a3b3d85510
hrana564 <hrana564@gmail.com>
2017-01-22 23:29:28 +0530

    fixed update cart issue pending only checkout

--- routes/customer/cart.js
+++ routes/customer/cart.js
@@ -4,29 +4,53 @@ var UserModel = require('../../models/user.js');
 var commonFunctions = require('../../utilities/functions.js');
 var config = require('../../utilities/config');
 var ProductModel = require('../../models/products');
+var mongoose = require('mongoose');
 
 
 router.get('/:alertClass/:alertMessage',function(request,response){
     if(commonFunctions.IsCustomerLoggedIn(request,response)){
-        ProductModel.find({},function (err,resource) {
-            if(err){
-                console.error(err);
-                response.render('customer/cart',{AppName:config.AppName,footerSignature:config.footerSignature,customMessageClass:"alert-danger",customMessage:"Internal server error occoured! Please try again later."});
-            } else{
-                response.render('customer/cart',{AppName:config.AppName,footerSignature:config.footerSignature,productList : resource,cartProducts : request.session.user.cartProducts,customMessageClass : request.params.alertClass,customMessage: request.params.alertMessage});
-            }
+        UserModel.findOne({_id: mongoose.Types.ObjectId(request.session.user._id)},function (err,resource) {
+            request.session.user = resource;
+            ProductModel.find({}, function (err, resource) {
+                if (err) {
+                    console.error(err);
+                    response.render('customer/cart', {
+                        AppName: config.AppName,
+                        footerSignature: config.footerSignature,
+                        customMessageClass: "alert-danger",
+                        customMessage: "Internal server error occoured! Please try again later."
+                    });
+                } else {
+                    response.render('customer/cart', {
+                        AppName: config.AppName,
+                        footerSignature: config.footerSignature,
+                        productList: resource,
+                        cartProducts: request.session.user.cartProducts,
+                        customMessageClass: request.params.alertClass,
+                        customMessage: request.params.alertMessage
+                    });
+                }
+            });
         });
     }
 });
 
 router.get('/',function(request,response){
     if(commonFunctions.IsCustomerLoggedIn(request,response)){
-        ProductModel.find({},function (err,resource) {
-            if(err){
-                response.redirect('/customer/cart/alert-danger/Internal server error occoured! Please try again later.');
-            } else{
-                response.render('customer/cart',{AppName:config.AppName,footerSignature:config.footerSignature,productList:resource,cartProducts : request.session.user.cartProducts});
-            }
+        UserModel.findOne({_id: mongoose.Types.ObjectId(request.session.user._id)},function (err,resource) {
+            request.session.user = resource;
+            ProductModel.find({}, function (err, resource) {
+                if (err) {
+                    response.redirect('/customer/cart/alert-danger/Internal server error occoured! Please try again later.');
+                } else {
+                    response.render('customer/cart', {
+                        AppName: config.AppName,
+                        footerSignature: config.footerSignature,
+                        productList: resource,
+                        cartProducts: request.session.user.cartProducts
+                    });
+                }
+            });
         });
     }
 });
@@ -34,15 +58,18 @@ router.get('/',function(request,response){
 router.post('/', function(request, response) {
     if(commonFunctions.IsCustomerLoggedIn(request,response)){
         if(request.body.productIDRFC){
-            UserModel.findOne({email:request.session.user.email},function (err,resource) {
-                resource.cartProducts.splice(resource.cartProducts.indexOf(request.body.productIDRFC),1);
-                resource.save(function (err,resource) {
-                   if(err){
-                       response.redirect('/customer/cart/alert-danger/Internal server error occoured! Please try again later.');
-                   } else{
-                       request.session.user = resource;
-                       response.redirect('/customer/cart/alert-success/Product was successfully removed from your cart.');
-                   }
+            UserModel.findOne({_id: mongoose.Types.ObjectId(request.session.user._id)},function (err,resource) {
+                request.session.user = resource;
+                UserModel.findOne({email: request.session.user.email}, function (err, resource) {
+                    resource.cartProducts.splice(resource.cartProducts.indexOf(request.body.productIDRFC), 1);
+                    resource.save(function (err, resource) {
+                        if (err) {
+                            response.redirect('/customer/cart/alert-danger/Internal server error occoured! Please try again later.');
+                        } else {
+                            request.session.user = resource;
+                            response.redirect('/customer/cart/alert-success/Product was successfully removed from your cart.');
+                        }
+                    });
                 });
             });
         }
