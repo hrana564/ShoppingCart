34e7a8711405d42c4c1c96b9c30525ca60ca351a
hrana564 <hrana564@gmail.com>
2017-01-28 23:41:16 +0530

    removed password form config files

--- utilities/config.js
+++ utilities/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    "database" : "mongodb://hrana564:Login123@ds111489.mlab.com:11489/shoppingcart",
+    "database" : "mongodb://hrana564:#password#@ds111489.mlab.com:11489/shoppingcart",
     "process": process.env.PORT || 3000,
     secretKey: "Your Secret Key",
     mailFrom : "hprana15051994@gmail.com",
-    emailSetup: "smtps://hprana15051994%40gmail.com:Login123@smtp.gmail.com",
+    emailSetup: "smtps://hprana15051994%40gmail.com:#passowrd#@smtp.gmail.com",
     AppName : "Online shopping application",
     hostName : "http://localhost:8000/",
     footerSignature : "Copyrights &copy; XYZ private ltd."
